27f241a8475b2b363f63593cac21cc50f1cff62f
peterxu35 <peter.px.xu@gmail.com>
2022-06-13 09:32:26 -0400

    server set up

---
+++ server.js
@@ -0,0 +1,24 @@
+require("dotenv").config()
+const express = require("express")
+const morgan = require("morgan")
+const methodOverride = require("method-override")
+const mongoose = require("mongoose")
+const path = require("path")
+
+const app = require("liquid-express-views")(express(), {
+    root: [path.resolve(__dirname, "views/")],
+  });
+
+  //middleware
+app.use(morgan("tiny")) 
+app.use(methodOverride("_method")) 
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static("public")) 
+
+app.get("/", (req, res) => {
+    res.send("Here");
+  });
+
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Listening on port 4000`)); 
