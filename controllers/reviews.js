5c0e02c2082f09882806056dfa285aa7bf5d12b9
peterxu35 <peter.px.xu@gmail.com>
2022-06-14 14:39:54 -0400

    add reviews controller

---
+++ controllers/reviews.js
@@ -0,0 +1,14 @@
+const Player = require('../models/player');
+
+module.exports = {
+  create
+};
+
+function create(req, res) {
+  Player.findById(req.params.id, function(err, movie) {
+    player.reviews.push(req.body);
+    player.save(function(err) {
+      res.redirect(`/players/${player._id}`);
+    });
+  });
+}
