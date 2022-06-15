dbbeb88ce2607ac3e5b4d40c91c18db403404b8d
peterxu35 <peter.px.xu@gmail.com>
2022-06-15 16:25:24 -0400

    edit review

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -2,6 +2,18 @@ const Player = require('../models/player');
 const express = require('express')
 const router = express.Router
 
+
+//Edit Route
+router.get("/:id/edit", (req, res) => {
+    Player.findById(req.params.id)
+    .then((player) => {
+        res.render("reviews/edit", {
+            reviews
+        })
+    })
+})
+
+//Create
 router.post('/players/:id/reviews', (req, res) => {
     Player.findById(req.params.id, function(err, player) {
         player.reviews.push(req.body);
