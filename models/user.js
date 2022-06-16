dcf136e49d2e95d76e3f41b097b674c9a2569713
peterxu35 <peter.px.xu@gmail.com>
2022-06-16 13:02:25 -0400

    heroku

--- models/user.js
+++ models/user.js
@@ -1,26 +1,26 @@
-// //////////////////////////////////////////////
-// // Import Dependencies
-// //////////////////////////////////////////////
-// const mongoose = require("./connections");
+//////////////////////////////////////////////
+// Import Dependencies
+//////////////////////////////////////////////
+const mongoose = require("./connections");
 
-// ////////////////////////////////////////////////
-// // Define Model
-// ////////////////////////////////////////////////
-// // pull schema and model from mongoose
-// const { Schema, model } = mongoose;
+////////////////////////////////////////////////
+// Define Model
+////////////////////////////////////////////////
+// pull schema and model from mongoose
+const { Schema, model } = mongoose;
 
-// // make user schema
-// const userSchema = new Schema({
-//   username: { type: String, required: true, unique: true },
-//   password: { type: String, required: true },
-//   playersList: [{type: Schema.Types.ObjectId, ref: 'Players'}]
-// });
+// make user schema
+const userSchema = new Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  playersList: [{type: Schema.Types.ObjectId, ref: 'Players'}]
+});
 
 
-// // make user model
-// const User = model("User", userSchema);
+// make user model
+const User = model("User", userSchema);
 
-// ///////////////////////////////////////////////////
-// // Export Model
-// ///////////////////////////////////////////////////
-// module.exports = User;
+///////////////////////////////////////////////////
+// Export Model
+///////////////////////////////////////////////////
+module.exports = User;
--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const methodOverride = require("method-override")
 const mongoose = require("./models/connections")
 const path = require("path")
 const PlayerRouter = require('./controllers/players')
-// const UserRouter = require('./controllers/users')
+const UserRouter = require('./controllers/users')
 const ReviewsRouter = require('./controllers/reviews')
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
 
 const app = require("liquid-express-views")(express(), {
     root: [path.resolve(__dirname, "views/")],
@@ -18,7 +20,16 @@ app.use(methodOverride("_method"))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static("public")) 
 
-// app.use('/user', UserRouter)
+app.use(
+    session({
+      secret: process.env.SECRET,
+      store: MongoStore.create({ mongoUrl: process.env.DATABASE_URL }),
+      saveUninitialized: true,
+      resave: false,
+    })
+  );
+
+app.use('/user', UserRouter)
 app.use('/players', PlayerRouter)
 app.use('/', ReviewsRouter)
 
